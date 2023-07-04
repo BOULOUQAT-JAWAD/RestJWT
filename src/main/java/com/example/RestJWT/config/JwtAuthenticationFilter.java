3a77f766519a136d55df6175c24c84f4642c13c7
BOULOUQAT-JAWAD <boulouqatjawad22@gmail.com>
2023-07-04 14:49:20 +0100

    first commit

--- src/main/java/com/example/RestJWT/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/RestJWT/config/JwtAuthenticationFilter.java
@@ -10,6 +10,8 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -53,8 +55,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                         null,
                         userDetails.getAuthorities()
                 );
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
-
+        filterChain.doFilter(request,response);
     }
 }
