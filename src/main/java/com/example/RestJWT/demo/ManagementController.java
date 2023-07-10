4766837326b5148062c93b59e9ef7baf3f5bddac
BOULOUQAT-JAWAD <boulouqatjawad22@gmail.com>
2023-07-10 10:03:48 +0100

    Authorization ADMIN MANAGER DONE

--- src/main/java/com/example/RestJWT/demo/ManagementController.java
+++ src/main/java/com/example/RestJWT/demo/ManagementController.java
@@ -3,10 +3,12 @@ package com.example.RestJWT.demo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/management")
+@PreAuthorize("hasAnyRole('ADMIN','MANAGER')")
 @Tag(name = "Management")
 public class ManagementController {
     @Operation(
@@ -25,18 +27,22 @@ public class ManagementController {
 
     )
     @GetMapping
+    @PreAuthorize("hasAnyAuthority('admin:read','manager:read')")
     public String get() {
         return "GET:: management controller";
     }
     @PostMapping
+    @PreAuthorize("hasAnyAuthority('admin:create','manager:create')")
     public String post() {
         return "POST:: management controller";
     }
     @PutMapping
+    @PreAuthorize("hasAnyAuthority('admin:update','manager:update')")
     public String put() {
         return "PUT:: management controller";
     }
     @DeleteMapping
+    @PreAuthorize("hasAnyAuthority('admin:delete','manager:delete')")
     public String delete() {
         return "DELETE:: management controller";
     }
